a62fc157e134b5c2b1df275c2813f5c8ad036ba9
Jad_Amm <Jadammydriss2005@gmail.com>
2025-10-06 09:11:47 +0200

    Test

---
+++ Trajet.cpp
@@ -0,0 +1,37 @@
+/*************************************************************************
+Xxx  -  description
+                             -------------------
+    début                : 2025
+    copyright            : (C) 3IFA par $AUTEUR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//------- Réalisation de la classe <Trajet> (fichier Trajet.cpp) ---------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Trajet.h"
+
+//----------------------------------------------------------------- PUBLIC
+
+//-------------------------------------------- Constructeurs - destructeur
+Trajet::Trajet ( )
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Trajet>" << endl;
+#endif
+} //----- Fin de Trajet
+
+
+Trajet::~Trajet ( )
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Trajet>" << endl;
+#endif
+} //----- Fin de ~Trajet
+
