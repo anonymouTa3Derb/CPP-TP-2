35770d4b3e8d78c2d7395652f0a3b4b4e34a7bba
[mehdi] <[essakim2000@gmail.com]>
2025-10-06 09:17:07 +0200

    Classe revisiter et corrigé

--- Trajet.h
+++ Trajet.h
@@ -27,50 +27,42 @@ public:
 //----------------------------------------------------- Méthodes publiques
     virtual void Afficher ( bool nl = true ) const = 0;
     // Mode d'emploi :
-    // Affiche sur la sortie standard une description du trajet.
-    // Le paramètre nl (nouvelle ligne) spécifie si l'affichage doit être
-    // suivi d'un retour à la ligne. Il y a retour à la ligne si nl = true
+    //
     // Contrat :
-    // Cette méthode doit être redéfinie dans les classes filles de Trajet
+    //
 
     virtual const char* GetDepart ( ) const = 0;
     // Mode d'emploi :
-    // Retourne un pointeur sur une chaine de caractères représentant la
-    // ville de départ pour ce trajet
+    //
     // Contrat :
-    // Cette méthode doit être redéfinie dans les classes filles de Trajet
-    // Le trajet doit posséder une ville de départ
+    //
 
     virtual const char* GetArrivee ( ) const = 0;
     // Mode d'emploi :
-    // Retourne un pointeur sur une chaine de caractères représentant la
-    // ville d'arrivée pour ce trajet
+    //
     // Contrat :
-    // Cette méthode doit être redéfinie dans les classes filles de Trajet
-    // Le trajet doit posséder une ville d'arrivée
+    //
 
     virtual Trajet* Clone ( ) const = 0;
     // Mode d'emploi :
-    // Retourne une copie (en profondeur) du trajet
+    //
     // Contrat :
-    // Cette méthode doit être redéfinie dans les classes filles de Trajet
+    //
 
 //------------------------------------------------- Surcharge d'opérateurs
-    Trajet& operator=(const Trajet& unTrajet);
 
 //-------------------------------------------- Constructeurs - destructeur
     Trajet ( );
     // Mode d'emploi :
-    // Constructeur appelé par les classes filles uniquement
-    // Cette classe (abstraite) n'est pas instanciable
+    //
     // Contrat :
-    // Rien
+    //
 
     virtual ~Trajet ( );
     // Mode d'emploi :
-    // Rien
+    //
     // Contrat :
-    // Rien
+    //
 
 //------------------------------------------------------------------ PRIVE
 
